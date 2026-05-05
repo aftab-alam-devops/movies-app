bb242f44ab4055dc7d87db853f8299039bf42c74
Aftab Alam <uxaftab@gmail.com>
2026-05-06 02:22:41 +0530

    movies app

--- frontend/src/component/SliderUtil.jsx
+++ frontend/src/component/SliderUtil.jsx
@@ -3,19 +3,27 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import MovieCard from "../pages/Movies/MovieCard";
 
-const SliderUtil = ({ data }) => {
+const SliderUtil = ({ data = [] }) => {
   const settings = {
     dots: true,
-    infinite: true,
+    infinite: data.length > 4,   // prevent crash if less items
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 2,
   };
 
+  // Safety check
+  if (!Array.isArray(data)) {
+    console.log("Invalid data passed to SliderUtil:", data);
+    return <div>No data available</div>;
+  }
+
   return (
     <Slider {...settings}>
-      {data?.map((movie) => (
-        <MovieCard key={movie._id} movie={movie} />
+      {data.map((movie) => (
+        <div key={movie._id}>
+          <MovieCard movie={movie} />
+        </div>
       ))}
     </Slider>
   );
